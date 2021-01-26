40cab8fee64210c9a3a7945a6303d0044f0911e3
luyuchen <lol@gmail.com>
2021-01-25 22:43:04 -0500

    Use godd config

--- meta.py
+++ meta.py
@@ -19,10 +19,10 @@ import pickle
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('batch_size', default=128, help='batch size')
-flags.DEFINE_integer('epochs', default=40, help='epoch train')
+flags.DEFINE_integer('epochs', default=50, help='epoch train')
 flags.DEFINE_integer('runs', default=10, help='nb runs')
 flags.DEFINE_integer('batchs_per_epoch', default=70, help='batchs in each epoch')
-flags.DEFINE_integer('teach_epochs', default=20, help='teaching epoch')
+flags.DEFINE_integer('teach_epochs', default=10, help='teaching epoch')
 
 
 def unwrap(var):
